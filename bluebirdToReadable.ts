054f39950e2f9677e1fcad2e15bd5eaf1e393dcd
Justin Beaudry <beaudry.justin@gmail.com>
2019-01-15 19:58:00 -0800

    adds husky, and lint-staged

--- bluebirdToReadable.ts
+++ bluebirdToReadable.ts
@@ -12,14 +12,14 @@ Bluebird.setScheduler((scheduler: (callback: CbAny) => void) => {
 });
 
 type MaybeErr = Error | null;
-type DestroyCallback = (error: ( MaybeErr )) => void;
+type DestroyCallback = (error: MaybeErr) => void;
 interface ReadableOptions {
   highWaterMark?: number;
   encoding?: string;
   objectMode?: boolean;
 }
 
-class BluebirdToReadableStream<T=any> extends Readable {
+class BluebirdToReadableStream<T = any> extends Readable {
   private promise: Bluebird<T>;
   constructor(promise: Bluebird<T>, options?: ReadableOptions) {
     super(options);
@@ -28,16 +28,16 @@ class BluebirdToReadableStream<T=any> extends Readable {
   public _read(): void {
     if (this.promise.isPending()) return;
     this.promise
-        .then((value: T) => {
-          if (!this.push(value)) {
-            this.emit('error', Error('Failed to push value'));
-          }
-          this.push(null);
-        })
-        .catch((err: Error) => {
-          this.emit('error', err);
-          this.push(null);
-        });
+      .then((value: T) => {
+        if (!this.push(value)) {
+          this.emit('error', Error('Failed to push value'));
+        }
+        this.push(null);
+      })
+      .catch((err: Error) => {
+        this.emit('error', err);
+        this.push(null);
+      });
   }
   public _destroy(err: MaybeErr, callback: DestroyCallback): void {
     // cancel the promise if it's still pending
@@ -46,6 +46,9 @@ class BluebirdToReadableStream<T=any> extends Readable {
   }
 }
 
-export function BluebirdToReadable<T>(promise: Bluebird<T>, options?: ReadableOptions) {
+export function BluebirdToReadable<T>(
+  promise: Bluebird<T>,
+  options?: ReadableOptions
+) {
   return Reflect.construct(BluebirdToReadableStream, [promise, options]);
 }
